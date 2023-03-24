d2d984ce211a20e6f8f078b52f9e72bb354785cf
Zack Osowski <62389933+zackosowski@users.noreply.github.com>
2023-03-24 13:39:52 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,9 +5,9 @@
 import main;
 import datetime;
 
-year = 2022
-month = 9
-day = 22
+year = 2023
+month = 4
+day = 1
 
 def test_code():
     assert main.divider(10, 2) == 5.0, " divider(10, 2) == 5.0 failed"
